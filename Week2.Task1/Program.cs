20861002d05af7d27843f036df1a309c8de8802b
Artem Grigoriev <artem.grigoryev@campus.tu-berlin.de>
2022-09-09 12:55:58 +0300

    week2

---
+++ Week2.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿string fullName = "Григорьев Артём";// Ф.И.О.
+int age = 24; // Возраст
+string email = "artist315.2010@gmail.com"; //Email
+float scoreProgramming = 100;// Баллы по программированию
+float scoreMath = 100; //Баллы по математике
+float scorePhisics = 100; //Баллы по физике
+
+Console.WriteLine($"Hi, my name is {fullName}.");
+Console.WriteLine($"I am {age} years old.");
+Console.WriteLine($"contact email: {email}.");
+Console.WriteLine($"My scores for programming, physics and math are {scoreProgramming}, {scoreMath}, {scorePhisics} respectivly.");
+Console.ReadKey(true);
---
+++ Week2.Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿float scoreProgramming = 34;// Баллы по программированию
+float scoreMath = 43; //Баллы по математике
+float scorePhisics = 54; //Баллы по физике
+
+Console.WriteLine("Press any key to calculate total score.");
+Console.ReadKey(true);
+
+var totalScore = scoreProgramming + scoreMath + scorePhisics;
+Console.WriteLine($"Total score: {totalScore}.");
+Console.WriteLine("Press any key to calculate average score.");
+Console.ReadKey(true);
+
+var avgScore = totalScore/3;
+Console.WriteLine($"Average score: {string.Format("{0:0.00}", avgScore) }.");
+Console.ReadKey(true);
+
