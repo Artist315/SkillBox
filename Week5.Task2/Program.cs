45a6bf821ffa14533bb708663f27abf529f66331
Artem Grigoriev <artem.grigoryev@campus.tu-berlin.de>
2022-12-10 23:02:36 +0300

    week5 task 2

---
+++ Week5.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Extensions;
+
+
+//asdaww awdadsdw 123 123 dqwea a a d 23 ea asdqw    asd wqe asd daw  asdwqad 
+var revertedSentence = ReversWords(InputExtesions.ReadStringInput("Введите строку"));
+
+
+
+OutputExtesions.PrintArray(revertedSentence);
+
+Console.ReadKey();
+
+static string[] ReversWords(string inputString)
+{
+    var stringArray = inputString.Split(' ');
+
+    string[] outputArray = new string[stringArray.Length];
+    for (int i = stringArray.Length-1; i >= 0; i--)
+    {
+        outputArray[stringArray.Length - 1 - i] = stringArray[i];
+    }
+    
+    return outputArray;
+}
