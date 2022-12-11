d2230ec908f75f7fac9a5aabf19974b3a8323af6
Artem Grigoriev <artem.grigoryev@campus.tu-berlin.de>
2022-12-11 15:21:15 +0300

    Week6 task1

--- Extensions/InputExtesions.cs
+++ Extensions/InputExtesions.cs
@@ -8,15 +8,36 @@ namespace Extensions
 {
     public static class InputExtesions
     {
-        public static int ReadIntInput(string message = null)
+        public static int ReadIntInput(string message = null, int[] approvedValues = null)
         {
-            Console.WriteLine($"{message}");
-            int input;
-            while (!int.TryParse(Console.ReadLine(), out input))
+            if (!string.IsNullOrEmpty(message))
             {
-                Console.WriteLine("Неверный формат, введите целое число");
+                Console.WriteLine($"{message}");
+            }
+            
+            int input;
+            while (!Check(Console.ReadLine(), out input))
+            {
+                if (approvedValues != null)
+                {
+                    Console.WriteLine("Неверный формат или число вне допустимого списка занчений");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат, введите целое число");
+                }
             }
             return input;
+
+            bool Check(string input, out int result)
+            {
+                var isInt = int.TryParse(input, out result);
+                if (approvedValues != null && isInt)
+                {
+                    isInt = approvedValues.Contains(result);
+                }
+                return isInt;
+            }
         }
 
         public static string ReadStringInput(string message = null)
---
+++ Week6.Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿using Extensions;
+
+string fileName = "Task1.txt";
+int? option;
+
+CheckIfExists(fileName);
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Выберите действие:");
+    Console.WriteLine("1 - Добавить запись сотрудника");
+    Console.WriteLine("2 - Показать существующие записи");
+    Console.WriteLine("0 - выход из программы");
+    Console.WriteLine();
+
+    option = InputExtesions.ReadIntInput(approvedValues : new int[] {1,2,0 });
+
+    switch (option)
+    {
+        case 1:
+            {
+                WriteToFile(fileName);
+                break;
+            }
+        case 2:
+            {
+                ReadFile(fileName);
+                break;
+            }
+        default:
+            break;
+    }
+    } while (option!=0);
+
+Console.ReadKey();
+
+void CheckIfExists(string fileName)
+{
+    if (!File.Exists(fileName))
+    {
+        File.Create(fileName).Close();
+    }    
+}
+
+void ReadFile(string fileName)
+{
+    using (StreamReader reader = new StreamReader(fileName))
+    {
+        string line;
+        do
+        {
+            line = reader.ReadLine();
+            line = line?.Replace("#", " ");
+            Console.WriteLine(line);
+        } while (line != null);
+        
+    }
+}
+
+void WriteToFile(string fileName)
+{
+    using (StreamWriter writer = new StreamWriter(fileName,true))
+    {
+        var input = InputExtesions.ReadStringInput("Введите данные:");
+        writer.WriteLine(input);
+        writer.Flush();
+    }
+}
