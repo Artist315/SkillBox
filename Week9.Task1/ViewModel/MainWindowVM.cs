c088fc5499dc54c996946f37267a54ff9ec1eff2
Artem Grigoriev <artist315.2010@gmail.com>
2023-02-05 13:34:29 +0300

    fix

--- Week9.Task1/ViewModel/MainWindowVM.cs
+++ Week9.Task1/ViewModel/MainWindowVM.cs
@@ -12,12 +12,7 @@ namespace Week9.Task1.ViewModel
             SplitLineCommand = new RelayCommand(o => SplitLine());
         }
 
-        private void SplitLine()
-        {
-            SplittedElements = Input.Split(" ").ToList();
-        }
-        public ICommand SplitLineCommand { get; set; }
-
+        #region Task2 revert line
         private string _input;
         public string Input
         {
@@ -28,6 +23,27 @@ namespace Week9.Task1.ViewModel
             set
             {
                 _input = value;
+            }
+        }
+        #endregion
+
+        #region Task1 split line
+        private void SplitLine()
+        {
+            SplittedElements = InputTask1.Split(" ").ToList();
+        }
+        public ICommand SplitLineCommand { get; set; }
+
+        private string _inputTask1;
+        public string InputTask1
+        {
+            get
+            {
+                return _inputTask1;
+            }
+            set
+            {
+                _inputTask1 = value;
             }
         }
         private List<string> _splittedElements = new List<string>();
@@ -43,5 +59,6 @@ namespace Week9.Task1.ViewModel
                 OnPropertyChanged();
             }
         }
+        #endregion
     }
 }
