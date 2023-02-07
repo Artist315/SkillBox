b16f6584eda120804907e951986906859d1811c9
Artem Grigoriev <artist315.2010@gmail.com>
2023-02-07 22:14:39 +0300

    week 10 build structure and add storage and customer manager

---
+++ Week10.Storage.Task1-3/Data/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10.Storage.Task1_3.Data
+{
+    public class Customer
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string MiddleName { get; private set; }
+        public int PassSeries { get; private set; }
+        public int PassNumber { get; private set; }
+    }
+}
